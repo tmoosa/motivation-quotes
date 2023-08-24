0893e3c468863f3f56eea985a4c0d9211259c9c6
Taher Moosa <lilt@Tahers-MacBook-Pro.local>
2023-08-24 12:19:53 -0500

    style done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./styles.css";
+import "./index.css";
 import Display from "./components/Display";
 import Button from "./components/Button";
 import axios from "axios";
